1a6426250f927785ac7ec44f1e318fc8de52a2ca
Lam Yuen Ho Joseph <yuenholam@gmail.com>
2023-05-10 23:18:12 +0800

    new solutions

---
+++ 59/main.py
@@ -0,0 +1,17 @@
+class Solution:
+    def generateMatrix(self, n: int) -> list[list[int]]:
+        matrix, low = [], n*n+1
+        while low > 1:
+            low, high = low - len(matrix), low
+            matrix = [list(range(low, high))] + list(map(lambda x: list(x), [*zip(*matrix[::-1])]))
+        return matrix
+        
+    
+testcases = []
+testcases.append((3, [[1,2,3],[8,9,4],[7,6,5]]))
+testcases.append((2, [[1,2],[4,3]]))
+testcases.append((1, [[1]]))
+
+solution = Solution()
+for input, output in testcases:
+    assert solution.generateMatrix(input) == output
