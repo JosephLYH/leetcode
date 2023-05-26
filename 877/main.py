023a27e1cb4043ee0d5995df2a52ee9babe61d84
Lam Yuen Ho Joseph <yuenholam@gmail.com>
2023-05-27 01:39:33 +0800

    new

---
+++ 877/main.py
@@ -0,0 +1,11 @@
+class Solution:
+    def stoneGame(self, piles: list[int]) -> bool:
+        return True
+    
+testcases = []
+testcases.append(([5,3,4,5], True))
+testcases.append(([3,7,2,3], True))
+
+solution = Solution()
+for testcase in testcases:
+    assert getattr(solution, dir(solution)[-1])(*testcase[:-1]) == testcase[-1]
