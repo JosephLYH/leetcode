b71e7aa73600413aa087a6cc8eb3ae354aaa098e
Lam Yuen Ho Joseph <yuenholam@gmail.com>
2023-06-11 23:52:08 +0800

    new problem

---
+++ code/71.py
@@ -0,0 +1,23 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        split_path = path.split('/')
+        new_path = []
+
+        for i in range(len(split_path)):
+            if not split_path[i] or split_path[i] == '.':
+                continue
+            elif split_path[i] == '..':
+                if new_path: new_path.pop()
+            else:
+                new_path.append(split_path[i])
+
+        return '/' + '/'.join(new_path)
+    
+testcases = []
+testcases.append(('/home/', '/home'))
+testcases.append(('/../', '/'))
+testcases.append(('/home//foo/', '/home/foo'))
+
+solution = Solution()
+for testcase in testcases:
+    assert getattr(solution, dir(solution)[-1])(*testcase[:-1]) == testcase[-1], testcase
