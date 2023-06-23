57f005ae56439574cda7f33096726ea9a8d50e03
Lam Yuen Ho Joseph <yuenholam@gmail.com>
2023-06-23 23:30:21 +0800

    changed template for easy debugging

--- template.py
+++ template.py
@@ -7,4 +7,6 @@ testcases = []
 solution = Solution()
 for testcase in testcases:
     output = getattr(solution, dir(solution)[-1])(*testcase[:-1])
-    assert output == testcase[-1], f'testcase: {testcase[:-1]}, expected: {testcase[-1]}, output: {output}'
+    if output != testcase[-1]:
+        getattr(solution, dir(solution)[-1])(*testcase[:-1])
+        assert False, f'testcase: {testcase[:-1]}, expected: {testcase[-1]}, output: {output}'
