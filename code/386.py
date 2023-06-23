f3e5e27152dd6b6a13717b6016ed3f406846ba11
Lam Yuen Ho Joseph <yuenholam@gmail.com>
2023-06-24 02:12:38 +0800

    new virtual contest

---
+++ code/386.py
@@ -0,0 +1,33 @@
+from typing import List
+
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        def dfs(num):
+            num = num * 10
+            if num > n:
+                return []
+
+            result = []
+            for i in range(num, num + 10):
+                if i <= n: 
+                    result.extend([i])
+                    result.extend(dfs(i))
+            
+            return result
+
+        result = []
+        for i in range(1, 10):
+            if i <= n: 
+                result.extend([i])
+                result.extend(dfs(i))
+
+        return result
+    
+testcases = []
+testcases.append((13, [1,10,11,12,13,2,3,4,5,6,7,8,9]))
+testcases.append((2, [1,2]))
+
+solution = Solution()
+for testcase in testcases:
+    output = getattr(solution, dir(solution)[-1])(*testcase[:-1])
+    assert output == testcase[-1], f'testcase: {testcase[:-1]}, expected: {testcase[-1]}, output: {output}'
---
+++ code/387.py
@@ -0,0 +1,26 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        seen = {}
+
+        for i in range(len(s)):
+            if s[i] in seen:
+                seen[s[i]] = -1
+            else:
+                seen[s[i]] = i
+
+        minimum = len(s)
+        for key in seen:
+            if seen[key] != -1:
+                minimum = min(minimum, seen[key])
+
+        return minimum if minimum != len(s) else -1
+    
+testcases = []
+testcases.append(('leetcode', 0))
+testcases.append(('loveleetcode', 2))
+testcases.append(('aabb', -1))
+
+solution = Solution()
+for testcase in testcases:
+    output = getattr(solution, dir(solution)[-1])(*testcase[:-1])
+    assert output == testcase[-1], f'testcase: {testcase[:-1]}, expected: {testcase[-1]}, output: {output}'
