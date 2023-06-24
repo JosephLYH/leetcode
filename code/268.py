6c4ea3a68dba6a245e8aa2d6950ac628fa356192
Lam Yuen Ho Joseph <yuenholam@gmail.com>
2023-06-24 11:58:20 +0800

    new problem

---
+++ code/268.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        return sum(range(len(nums) + 1)) - sum(nums)
+    
+testcases = []
+testcases.append([[3,0,1], 2])
+testcases.append([[0,1], 2])
+testcases.append([[9,6,4,2,3,5,7,0,1], 8])
+
+solution = Solution()
+for testcase in testcases:
+    output = getattr(solution, dir(solution)[-1])(*testcase[:-1])
+    if output != testcase[-1]:
+        getattr(solution, dir(solution)[-1])(*testcase[:-1])
+        assert False, f'testcase: {testcase[:-1]}, expected: {testcase[-1]}, output: {output}'
