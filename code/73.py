f2403cf857965033a4162741b68340f1083b625b
Lam Yuen Ho Joseph <yuenholam@gmail.com>
2023-06-24 12:09:00 +0800

    new problem

---
+++ code/73.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        xs = set()
+        ys = set()
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    xs.add(j)
+                    ys.add(i)
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if i in ys or j in xs:
+                    matrix[i][j] = 0
+
+testcases = []
+
+solution = Solution()
+for testcase in testcases:
+    output = getattr(solution, dir(solution)[-1])(*testcase[:-1])
+    if output != testcase[-1]:
+        getattr(solution, dir(solution)[-1])(*testcase[:-1])
+        assert False, f'testcase: {testcase[:-1]}, expected: {testcase[-1]}, output: {output}'
