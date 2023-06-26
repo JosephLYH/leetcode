25fead0b76a47296d3721e3f2947977f11b475fa
Lam Yuen Ho Joseph <yuenholam@gmail.com>
2023-06-27 00:31:40 +0800

    new problem

---
+++ code/2336.py
@@ -0,0 +1,17 @@
+class SmallestInfiniteSet:
+    def __init__(self):
+        self.added = set()
+        self.current = 1
+
+    def popSmallest(self) -> int:
+        if self.added:
+            temp = min(self.added)
+            self.added.remove(temp)
+            return temp
+        
+        self.current += 1
+        return self.current - 1
+
+    def addBack(self, num: int) -> None:
+        if self.current > num:
+            self.added.add(num)
