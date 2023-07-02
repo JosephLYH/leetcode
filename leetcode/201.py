224a892e0c2fed2efca82c76b7218cebd8cc3d99
Lam Yuen Ho Joseph <yuenholam@gmail.com>
2023-07-02 19:59:11 +0800

    new

---
+++ leetcode/201.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        while right > left:
+            right &= right - 1
+        return right
+
+
+testcases = []
+testcases.append((5, 7, 4))
+testcases.append((0, 0, 0))
+testcases.append((1, 2147483647, 0))
+
+solution = Solution()
+for testcase in testcases:
+    output = getattr(solution, dir(solution)[-1])(*testcase[:-1])
+    if output != testcase[-1]:
+        getattr(solution, dir(solution)[-1])(*testcase[:-1])
+        assert (
+            False
+        ), f"testcase: {testcase[:-1]}, expected: {testcase[-1]}, output: {output}"
